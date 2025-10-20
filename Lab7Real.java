861b8dd57056ba89a0dac927b987d9ec5094082d
Aswinth Sinnathamby <aswinthsinnathamby@gmail.com>
2025-10-20 08:21:39 -0400

    First commit Test

---
+++ Lab7Real.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab7real;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author 2472557
+ */
+public class Lab7Real extends Application {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage stage) {
+        
+    }
+    
+}
