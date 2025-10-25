8f87f9acec992b4ad574f952e18cf2f2da0fa8e9
Aswinth Sinnathamby <aswinthsinnathamby@gmail.com>
2025-10-25 19:29:01 -0400

    Refactor path animation with new shapes and controls
    Updated the rectangle and circle dimensions, added a path for the circle to follow, and included buttons for start, reset, and exit functionality.

--- Lab7Real.java
+++ Lab7Real.java
@@ -37,31 +37,51 @@ public class Lab7Real extends Application {
     @Override
     public void start(Stage stage) {
         Pane pane = new Pane();
-        Rectangle rectangle = new Rectangle(100, 100, 400, 400);
-        rectangle.setStroke(Color.WHITE);
+        Rectangle rectangle = new Rectangle(50, 50, 400, 400);
+        rectangle.setStroke(Color.BLACK);
         rectangle.setFill(null);
-        Circle circle = new Circle(100, 100, 50);
-        circle.setFill(Color.WHITE);
+        
+        Circle circle = new Circle(50, 50, 25);
+        circle.setFill(Color.BLACK);
         circle.setStroke(Color.BLACK);
+        circle.setTranslateX(100);
+        circle.setTranslateY(100);
+        
         pane.getChildren().addAll(circle, rectangle);
+        
+        Path path = new Path();
+        path.getElements().add(new MoveTo(50, 50)); // M
+        path.getElements().add(new LineTo(450, 50)); // N
+        path.getElements().add(new LineTo(450, 450)); // P
+        path.getElements().add(new LineTo(50, 450)); // Q
+        path.getElements().add(new LineTo(50, 50)); // M
 
-        PathTransition pt = new PathTransition();
-        pt.setDuration(Duration.millis(4000));
-        pt.setPath(rectangle);
+        PathTransition pt = new PathTransition(Duration.seconds(8), path, circle);
+        pt.setCycleCount(1);
+        pt.setDuration(Duration.seconds(8));
+        pt.setPath(path);
         pt.setNode(circle);
-
+        //pt.setOrientation(PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT);
         pt.setCycleCount(Timeline.INDEFINITE);
-        pt.play(); // Start animation
-        circle.setOnMousePressed(e -> pt.pause());
-        circle.setOnMouseReleased(e -> pt.play());
+        pt.play(); // Start animation;
 
-        Scene scene = new Scene(pane, 600, 600);
+        Ellipse ellipse = new Ellipse(250, 250, 50, 25);
+        ellipse.setFill(Color.WHITE);
+        ellipse.setStroke(Color.BLACK);
+        pane.getChildren().addAll(ellipse);
+        
+        Button start = new Button("Start");
+        Button reset = new Button("Reset");
+        Button exit = new Button("Exit");
+
+        Scene scene = new Scene(pane, 600, 500);
         stage.setScene(scene);
         stage.setTitle("Path animation");
         stage.show();
     }
 
 }
+
 
 
 
